d9dbf6854a48107838b99dc358703d63ab6185eb
Khaifa Gifari <khaifag99@gmail.com>
2019-04-23 23:08:38 +0700

    Update detail_produk.php

--- application/views/home/detail_produk.php
+++ application/views/home/detail_produk.php
@@ -83,8 +83,8 @@ height: 100%;">
 	<?php foreach ($komentar as $komen): ?>
 		<a href="<?php echo base_url('komentar/daftarKomentar').$kopi['id_menu'] ?>"> </a>
 		<dl class="item-property">
-			<p style="font-size:14px;color: grey"> Oleh: <a href="" style="color:black;font-size: 17px; font-style: italic;" > <?php echo $komen['nama_komentar'] ?> </a></p>
-			<p style="font-size: 20px;"><?php echo $komen['isi_komentar'] ?> </p>
+			<p class="mt-3 ml-3" style="font-size:14px;color: grey"> Oleh: <a href="" style="color:black;font-size: 17px; font-style: italic;" > <?php echo $komen['nama_komentar'] ?> </a></p>
+			<p class="mt-3 ml-3" style="font-size: 20px;"><?php echo $komen['isi_komentar'] ?> </p>
 		</dl>
 		<hr>
 	<?php endforeach; ?>
